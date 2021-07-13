87f1c75fe8d2f07349952c8708ca0b40b446438d
srikanthpragada <srikanthpragada@gmail.com>
2021-07-13 08:26:52 +0530

    13-JULY

---
+++ demo/oop/exh_demo.py
@@ -0,0 +1,14 @@
+try:
+    v = input("Enter count :")
+    count = int(v)
+    print('Share :', 1000 / count)
+except ValueError:
+    print("Invalid input. Please provide a valid number!")
+else:
+    print("Else")
+# except ZeroDivisionError:
+#     print("Sorry! Count cannot be zero!")
+finally:
+    print("Finally")
+
+print("The End")
